8b51a1cbd1490008e7f9ceb25883f71f7f6c6880
TonyWu <tony.wu@hiii.com.tw>
2025-08-05 15:03:53 +0800

    first commit

---
+++ website/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="hero">
+      <h1>Dashcam 行車紀錄器 App</h1>
+      <p>好用的行車紀錄器應用程式，安全守護、精彩回顧</p>
+      <a
+        className="btn"
+        href="https://apps.apple.com/tw/app/%E4%BB%8A%E9%A7%95%E6%9C%89%E5%BD%B1-%E6%89%8B%E6%A9%9F%E8%A1%8C%E8%BB%8A%E8%BC%94%E5%8A%A9%E8%A8%98%E9%8C%84%E5%99%A8/id6746118261"
+        target="_blank"
+        rel="noreferrer"
+      >
+        立即下載
+      </a>
+    </div>
+  );
+} 
